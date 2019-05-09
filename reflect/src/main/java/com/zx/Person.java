cb5ce5b5ad5675e3dda8c28ce2d91a98430ef369
zhangxiang0903 <zx19870903@163.com>
2019-05-09 08:29:55 +0800

    mybatis学习代码

---
+++ reflect/src/main/java/com/zx/Person.java
@@ -0,0 +1,42 @@
+package com.zx;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void test(String name,int age){
+        System.out.println("heihei");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
